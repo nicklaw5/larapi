f0755482726fdbbb9dd00e8611ebd5d7756a85bf
Nick Law <nick@nicholaslaw.com.au>
2015-11-07 08:23:00 +1100

    first commit

---
+++ src/LarapiServiceProvider.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Nicklaw5\Larapi\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        //
+    }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['larapi'] = $this->app->share(function($app){
+			return new Larapi;
+		});
+		$this->app->booting(function(){
+			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias('Larapi', 'Nicklaw5\Larapi\Facades\Larapi');
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('larapi');
+	}
+
+}
